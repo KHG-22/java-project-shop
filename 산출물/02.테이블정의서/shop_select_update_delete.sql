30b6ff9ab7cccea81497c095ede09ccf2590d49b
academyitwill <academyitwill@gmail.com>
2025-03-24 16:28:44 +0900

    ..

--- 산출물/02.테이블정의서/shop_select_update_delete.sql
+++ 산출물/02.테이블정의서/shop_select_update_delete.sql
@@ -1,7 +1,7 @@
 /*****************member****************/
---로그인한 회원정보수정
+--update by pk[로그인한 회원정보수정]
 update userinfo set password = 'password', name= 'cname', email='cemail' where userid='guard1';
---로그인한 회원의 정보보기
+--select by pk[로그인한 회원의 정보보기]
 select * from userinfo where userid='guard1';
 
 
