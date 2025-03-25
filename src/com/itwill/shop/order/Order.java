f6e9ed3b3929891b2062b9574f905f09fb41008e
academyitwill <academyitwill@gmail.com>
2025-03-25 16:04:14 +0900

    ..

--- src/com/itwill/shop/order/Order.java
+++ src/com/itwill/shop/order/Order.java
@@ -1,10 +1,4 @@
 package com.itwill.shop.order;
-
-import java.util.Date;
-import java.util.List;
-
-import com.itwill.shop.user.User;
-
 /*
 이름      널?       유형            
 ------- -------- ------------- 
@@ -12,17 +6,86 @@ O_NO    NOT NULL NUMBER(10)
 O_DESC           VARCHAR2(100) 
 O_DATE           DATE          
 O_PRICE          NUMBER(10)    
-USERID           VARCHAR2(50)  
+USERID           VARCHAR2(100) 
  */
+
+import java.util.Date;
+import java.util.List;
+
+import com.itwill.shop.user.User;
+
 public class Order {
 	private int o_no;
 	private String o_desc;
 	private Date o_date;
 	private int o_price;
-	/**********FK***********/
+	/**********FK************/
 	//private String userid;
 	private User user;
 	
 	private List<OrderItem> orderItems;
+	
+	public Order() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Order(int o_no, String o_desc, Date o_date, int o_price, User user, List<OrderItem> orderItems) {
+		super();
+		this.o_no = o_no;
+		this.o_desc = o_desc;
+		this.o_date = o_date;
+		this.o_price = o_price;
+		this.user = user;
+		this.orderItems = orderItems;
+	}
+
+	public int getO_no() {
+		return o_no;
+	}
+
+	public void setO_no(int o_no) {
+		this.o_no = o_no;
+	}
+
+	public String getO_desc() {
+		return o_desc;
+	}
+
+	public void setO_desc(String o_desc) {
+		this.o_desc = o_desc;
+	}
+
+	public Date getO_date() {
+		return o_date;
+	}
+
+	public void setO_date(Date o_date) {
+		this.o_date = o_date;
+	}
+
+	public int getO_price() {
+		return o_price;
+	}
+
+	public void setO_price(int o_price) {
+		this.o_price = o_price;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(List<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+	
 	
 }
--- src/com/itwill/shop/order/OrderItem.java
+++ src/com/itwill/shop/order/OrderItem.java
@@ -1,7 +1,4 @@
 package com.itwill.shop.order;
-
-import com.itwill.shop.product.Product;
-
 /*
 이름     널?       유형         
 ------ -------- ---------- 
@@ -10,14 +7,52 @@ OI_QTY          NUMBER(10)
 O_NO            NUMBER(10) 
 P_NO            NUMBER(10) 
  */
+
+import com.itwill.shop.product.Product;
+
 public class OrderItem {
 	private int oi_no;
 	private int oi_qty;
-	/******FK***********/
+	/********FK***********/
 	//private int o_no;
-	Order order;
-	/******FK***********/
+	private Order order;
+	/********FK***********/
 	//private int p_no;
-	Product product;
-
+	private Product product;
+	public OrderItem() {
+		// TODO Auto-generated constructor stub
+	}
+	public OrderItem(int oi_no, int oi_qty, Order order, Product product) {
+		super();
+		this.oi_no = oi_no;
+		this.oi_qty = oi_qty;
+		this.order = order;
+		this.product = product;
+	}
+	public int getOi_no() {
+		return oi_no;
+	}
+	public void setOi_no(int oi_no) {
+		this.oi_no = oi_no;
+	}
+	public int getOi_qty() {
+		return oi_qty;
+	}
+	public void setOi_qty(int oi_qty) {
+		this.oi_qty = oi_qty;
+	}
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	
 }
