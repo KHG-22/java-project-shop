2a19e3076b2165195efdc7357f5cc2a4e6a14679
academyitwill <academyitwill@gmail.com>
2025-03-25 11:15:51 +0900

    .

--- 산출물/02.테이블정의서/shop_select_update_delete.sql
+++ 산출물/02.테이블정의서/shop_select_update_delete.sql
@@ -1,32 +1,40 @@
-/*****************member****************/
---update by pk[로그인한 회원정보수정]
-update userinfo set password = 'password', name= 'cname', email='cemail' where userid='guard1';
---select by pk[로그인한 회원의 정보보기]
+/**********************member************************/
+--update by pk[로그인한회원정보수정]
+update userinfo set password='password',name='cname',email ='cemail' where userid='guard1';
+--select by pk[로그인한회원의 정보보기]
 select * from userinfo where userid='guard1';
 
 
-/********************product*****************/
-
+/**********************product************************/
 --select by pk
 select * from product where p_no=1;
 --select all
 select * from product;
 
 
-/*****************cart***********************/
+/*********************cart*************************/
 --guard1회원 카트에 제품번호 존재여부
-select count(*) as p_count from cart where userid='guard1' and p_no = 8;
+select count(*)  as p_count from cart where userid='guard1' and p_no = 8;
+/*
+-----
+p_count
+------
+ 0
+*/
+
+select count(*)  as p_count from cart where userid='guard1' and p_no =1;
+/*
+-----
+p_count
+------
+ 1
+*/
+--guard1 회원카트에있는 1번제품의 수량 1개증가수정
+update cart set cart_qty = cart_qty+1 where userid='guard1' and p_no=1;
+--guard1 회원카트에있는 1번제품의 수량 3개로수정
+update cart set cart_qty = 3 where userid='guard1' and p_no=1;
 
 
-
-
-
---guard1 회원카트아이템리스트
-
-select * from cart c join product p on c.p_no=p.p_no where userid = 'guard1';
-
---guard1 회원카트에 있는 1번제품의 수량증가
-update cart set cart_qty = cart_qty+1 where userid='guard1' and p_no =1;
-
---guard1 회원카트에 있는 1번제품의 수량 3개로 수정
-update cart set cart_qty = 3 where userid='guard1' and p_no =1;
+-- guard1 회원카트아이템리스트
+select * from cart c join product p on c.p_no=p.p_no 
+where userid='guard1';
