b065ea67adac5840e616f7d334397430debbbcc7
academyitwill <academyitwill@gmail.com>
2025-03-25 11:13:03 +0900

    .

--- 산출물/02.테이블정의서/shop_select_update_delete.sql
+++ 산출물/02.테이블정의서/shop_select_update_delete.sql
@@ -14,7 +14,19 @@ select * from product;
 
 
 /*****************cart***********************/
+--guard1회원 카트에 제품번호 존재여부
+select count(*) as p_count from cart where userid='guard1' and p_no = 8;
+
+
+
+
+
 --guard1 회원카트아이템리스트
 
 select * from cart c join product p on c.p_no=p.p_no where userid = 'guard1';
 
+--guard1 회원카트에 있는 1번제품의 수량증가
+update cart set cart_qty = cart_qty+1 where userid='guard1' and p_no =1;
+
+--guard1 회원카트에 있는 1번제품의 수량 3개로 수정
+update cart set cart_qty = 3 where userid='guard1' and p_no =1;
