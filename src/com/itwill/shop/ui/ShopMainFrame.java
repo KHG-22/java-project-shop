cf45bdf36618e5407950ac577c527c75c412d069
academyitwill <academyitwill@gmail.com>
2025-03-27 10:10:25 +0900

    ..

--- src/com/itwill/shop/ui/ShopMainFrame.java
+++ src/com/itwill/shop/ui/ShopMainFrame.java
@@ -5,11 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JTabbedPane;
 
 public class ShopMainFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTabbedPane shopTabbedPane;
 
 	/**
 	 * Launch the application.
@@ -32,11 +35,30 @@ public class ShopMainFrame extends JFrame {
 	 */
 	public ShopMainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 400, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel northSearchPanel = new JPanel();
+		contentPane.add(northSearchPanel, BorderLayout.NORTH);
+		
+		JPanel southMenuPanel = new JPanel();
+		contentPane.add(southMenuPanel, BorderLayout.SOUTH);
+		
+		shopTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(shopTabbedPane, BorderLayout.CENTER);
+		
+		JTabbedPane memberTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		shopTabbedPane.addTab("New tab", null, memberTabbedPane, null);
+		
+		JPanel memberJoinPanel = new JPanel();
+		memberTabbedPane.addTab("New tab", null, memberJoinPanel, null);
+		
+		JPanel memberLoginPanel = new JPanel();
+		memberTabbedPane.addTab("New tab", null, memberLoginPanel, null);
 	}
 
 }
